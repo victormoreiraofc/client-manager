91f570adfb629c0ebaab404df369028e06049087
Victor Moreira <victormoreiraoficial@gmail.com>
2024-09-29 01:53:27 -0300

    ✨ feat: Esqueceu a senha e mostrar ou ocultar a senha

--- src/telas/TelaLogin.java
+++ src/telas/TelaLogin.java
@@ -52,36 +52,60 @@ public class TelaLogin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jCheckBox1 = new javax.swing.JCheckBox();
+        jlibEsqueceuASenha = new javax.swing.JLabel();
+        btnEsqueceuASenha = new javax.swing.JToggleButton();
+        chbMostrarSenha = new javax.swing.JCheckBox();
         txtLogin = new javax.swing.JTextField();
         btnLogin = new javax.swing.JButton();
         txtSenha = new javax.swing.JPasswordField();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        jilbEmailOuUsuario = new javax.swing.JLabel();
+        jilbSenha = new javax.swing.JLabel();
         chbLembre = new javax.swing.JCheckBox();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
+        jilbAindaNaoTemConta = new javax.swing.JLabel();
+        jilbRegistreSe = new javax.swing.JLabel();
         btnResgistrar = new javax.swing.JButton();
         Background = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Sistema da CTCONTAB");
         getContentPane().setLayout(null);
 
-        jCheckBox1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/olho-aberto.png"))); // NOI18N
-        jCheckBox1.setInheritsPopupMenu(true);
-        jCheckBox1.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/images/olho-fechado.png"))); // NOI18N
-        jCheckBox1.setRolloverSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/images/olho-fechado.png"))); // NOI18N
-        jCheckBox1.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/images/olho-fechado.png"))); // NOI18N
-        jCheckBox1.addActionListener(new java.awt.event.ActionListener() {
+        jlibEsqueceuASenha.setFont(new java.awt.Font("SansSerif", 1, 10)); // NOI18N
+        jlibEsqueceuASenha.setForeground(new java.awt.Color(194, 166, 40));
+        jlibEsqueceuASenha.setText("Esqueceu a senha?");
+        getContentPane().add(jlibEsqueceuASenha);
+        jlibEsqueceuASenha.setBounds(590, 300, 100, 30);
+
+        btnEsqueceuASenha.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jCheckBox1ActionPerformed(evt);
+                btnEsqueceuASenhaActionPerformed(evt);
             }
         });
-        getContentPane().add(jCheckBox1);
-        jCheckBox1.setBounds(510, 230, 30, 28);
-        getContentPane().add(txtLogin);
-        txtLogin.setBounds(270, 130, 280, 60);
+        getContentPane().add(btnEsqueceuASenha);
+        btnEsqueceuASenha.setBounds(580, 300, 115, 27);
 
+        chbMostrarSenha.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/olho-aberto.png"))); // NOI18N
+        chbMostrarSenha.setInheritsPopupMenu(true);
+        chbMostrarSenha.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/images/olho-fechado.png"))); // NOI18N
+        chbMostrarSenha.setRolloverSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/images/olho-fechado.png"))); // NOI18N
+        chbMostrarSenha.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/images/olho-fechado.png"))); // NOI18N
+        chbMostrarSenha.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                chbMostrarSenhaActionPerformed(evt);
+            }
+        });
+        getContentPane().add(chbMostrarSenha);
+        chbMostrarSenha.setBounds(510, 230, 30, 28);
+
+        txtLogin.setBackground(new java.awt.Color(4, 21, 57));
+        txtLogin.setFont(new java.awt.Font("SansSerif", 1, 12)); // NOI18N
+        txtLogin.setForeground(new java.awt.Color(255, 255, 255));
+        getContentPane().add(txtLogin);
+        txtLogin.setBounds(270, 150, 280, 50);
+
+        btnLogin.setBackground(new java.awt.Color(194, 166, 40));
+        btnLogin.setFont(new java.awt.Font("SansSerif", 1, 12)); // NOI18N
+        btnLogin.setForeground(new java.awt.Color(255, 255, 255));
         btnLogin.setText("Login");
         btnLogin.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -89,8 +113,11 @@ public class TelaLogin extends javax.swing.JFrame {
             }
         });
         getContentPane().add(btnLogin);
-        btnLogin.setBounds(300, 310, 210, 40);
+        btnLogin.setBounds(300, 310, 220, 40);
 
+        txtSenha.setBackground(new java.awt.Color(4, 21, 57));
+        txtSenha.setFont(new java.awt.Font("SansSerif", 1, 12)); // NOI18N
+        txtSenha.setForeground(new java.awt.Color(255, 255, 255));
         txtSenha.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtSenhaActionPerformed(evt);
@@ -99,16 +126,19 @@ public class TelaLogin extends javax.swing.JFrame {
         getContentPane().add(txtSenha);
         txtSenha.setBounds(270, 220, 280, 50);
 
-        jLabel1.setForeground(new java.awt.Color(194, 166, 40));
-        jLabel1.setText("Email ou Usuário:");
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(270, 110, 190, 16);
+        jilbEmailOuUsuario.setFont(new java.awt.Font("SansSerif", 1, 12)); // NOI18N
+        jilbEmailOuUsuario.setForeground(new java.awt.Color(194, 166, 40));
+        jilbEmailOuUsuario.setText("Email ou Usuário:");
+        getContentPane().add(jilbEmailOuUsuario);
+        jilbEmailOuUsuario.setBounds(270, 130, 190, 16);
 
-        jLabel2.setForeground(new java.awt.Color(194, 166, 40));
-        jLabel2.setText("Senha:");
-        getContentPane().add(jLabel2);
-        jLabel2.setBounds(270, 200, 170, 16);
+        jilbSenha.setFont(new java.awt.Font("SansSerif", 1, 12)); // NOI18N
+        jilbSenha.setForeground(new java.awt.Color(194, 166, 40));
+        jilbSenha.setText("Senha:");
+        getContentPane().add(jilbSenha);
+        jilbSenha.setBounds(270, 200, 170, 20);
 
+        chbLembre.setFont(new java.awt.Font("SansSerif", 1, 12)); // NOI18N
         chbLembre.setForeground(new java.awt.Color(194, 166, 40));
         chbLembre.setText("Lembre-me");
         chbLembre.addActionListener(new java.awt.event.ActionListener() {
@@ -117,18 +147,20 @@ public class TelaLogin extends javax.swing.JFrame {
             }
         });
         getContentPane().add(chbLembre);
-        chbLembre.setBounds(270, 280, 85, 20);
+        chbLembre.setBounds(270, 280, 100, 20);
 
-        jLabel4.setBackground(new java.awt.Color(255, 255, 255));
-        jLabel4.setForeground(new java.awt.Color(115, 115, 115));
-        jLabel4.setText("Ainda não tem conta?");
-        getContentPane().add(jLabel4);
-        jLabel4.setBounds(310, 360, 190, 20);
+        jilbAindaNaoTemConta.setBackground(new java.awt.Color(255, 255, 255));
+        jilbAindaNaoTemConta.setFont(new java.awt.Font("SansSerif", 1, 12)); // NOI18N
+        jilbAindaNaoTemConta.setForeground(new java.awt.Color(115, 115, 115));
+        jilbAindaNaoTemConta.setText("Ainda não tem conta?");
+        getContentPane().add(jilbAindaNaoTemConta);
+        jilbAindaNaoTemConta.setBounds(310, 350, 130, 20);
 
-        jLabel3.setForeground(new java.awt.Color(194, 166, 40));
-        jLabel3.setText("Registre-se");
-        getContentPane().add(jLabel3);
-        jLabel3.setBounds(430, 360, 80, 20);
+        jilbRegistreSe.setFont(new java.awt.Font("SansSerif", 1, 12)); // NOI18N
+        jilbRegistreSe.setForeground(new java.awt.Color(194, 166, 40));
+        jilbRegistreSe.setText("Registre-se");
+        getContentPane().add(jilbRegistreSe);
+        jilbRegistreSe.setBounds(440, 350, 70, 20);
 
         btnResgistrar.setBackground(new java.awt.Color(30, 30, 30));
         btnResgistrar.setContentAreaFilled(false);
@@ -138,7 +170,7 @@ public class TelaLogin extends javax.swing.JFrame {
             }
         });
         getContentPane().add(btnResgistrar);
-        btnResgistrar.setBounds(420, 360, 100, 20);
+        btnResgistrar.setBounds(440, 350, 70, 20);
 
         Background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/background-contabil.png"))); // NOI18N
         Background.setText("jLabel3");
@@ -187,9 +219,18 @@ public class TelaLogin extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_txtSenhaActionPerformed
 
-    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jCheckBox1ActionPerformed
+    private void chbMostrarSenhaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_chbMostrarSenhaActionPerformed
+        if (chbMostrarSenha.isSelected()) {
+            txtSenha.setEchoChar((char) 0);
+        } else {
+            txtSenha.setEchoChar('•');
+        }
+    }//GEN-LAST:event_chbMostrarSenhaActionPerformed
+
+    private void btnEsqueceuASenhaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEsqueceuASenhaActionPerformed
+        dispose();
+       new TelaRegistro().setVisible(true);
+    }//GEN-LAST:event_btnEsqueceuASenhaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -228,14 +269,16 @@ public class TelaLogin extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Background;
+    private javax.swing.JToggleButton btnEsqueceuASenha;
     private javax.swing.JButton btnLogin;
     private javax.swing.JButton btnResgistrar;
     private javax.swing.JCheckBox chbLembre;
-    private javax.swing.JCheckBox jCheckBox1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
+    private javax.swing.JCheckBox chbMostrarSenha;
+    private javax.swing.JLabel jilbAindaNaoTemConta;
+    private javax.swing.JLabel jilbEmailOuUsuario;
+    private javax.swing.JLabel jilbRegistreSe;
+    private javax.swing.JLabel jilbSenha;
+    private javax.swing.JLabel jlibEsqueceuASenha;
     private javax.swing.JTextField txtLogin;
     private javax.swing.JPasswordField txtSenha;
     // End of variables declaration//GEN-END:variables
